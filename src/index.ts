849823cbae2406d276344c0672924e9bb71440b8
Rafael Castro <rafael.castro1002@gmail.com>
2022-04-14 20:38:39 -0400

    fixing remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import generateAppliedDosesChart from "./generateAppliedDosesChart";
 import generateSentDosesChart from "./generateSentDosesChart";
 
-// generateAppliedDosesChart();
+generateAppliedDosesChart();
 
 generateSentDosesChart();
